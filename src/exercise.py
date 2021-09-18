53eac0cffc289181e4f6796ef034a5f7ad82c67c
drknsblct <57183092+drknsblct@users.noreply.github.com>
2021-09-19 02:54:16 +0300

    Update exercise.py

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,16 @@
 def main():
     #write your code below this line
+    while True:
+        num = int(input('Give a number:'))
+        if num == 0:
+            break
+            
+        elif num < 0:
+            print('Unsuitable number')
+            continue
+            
+        else:
+            print(num**2)
 
 if __name__ == '__main__':
     main()
