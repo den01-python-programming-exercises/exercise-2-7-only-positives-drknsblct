5c89ee7476a9742b02f5b110ed0b8cc039ce3702
drknsblct <57183092+drknsblct@users.noreply.github.com>
2021-09-19 03:58:33 +0300

    Update exercise.py

--- src/exercise.py
+++ src/exercise.py
@@ -1,16 +1,19 @@
+
 def main():
     #write your code below this line
+
     while True:
-        number = int(input("Give a number:"))
-
-        if (number == 0):
+        num = int(input('Give a number:'))
+        if num == 0:
             break
+            
+        # it's cleaner to use an if statement instead of an elif 
+        # because if the above statement is true the loop breaks so both ifs won't be executed
+        if num < 0:
+            print('Unsuitable number')
 
-        if (number <= 0):
-            print("Unsuitable number")
-            continue
-
-        print(number * number)
+        else:
+            print(num**2)
 
 if __name__ == '__main__':
     main()
